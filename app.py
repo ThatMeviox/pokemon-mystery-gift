047a52ebee0655faa1846a9e6a19847e73991eeb
ThatMeviox <gmaniek1992@gmail.com>
2026-01-14 15:30:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
-// To musi być w Twoim pliku index.js lub app.js na serwerze
-app.get('/ping', (req, res) => {
-    res.status(200).send('pong');
-});
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# To jest główna strona - opcjonalna
+@app.route('/')
+def home():
+    return "Serwer PokéNet Działa!"
+
+# TO JEST TO, CZEGO SZUKA TWOJA APKA (kod 404 zamieni się w 200)
+@app.route('/ping')
+def ping():
+    return "pong", 200
+
+if __name__ == '__main__':
+    # Render wymaga, żeby serwer słuchał na porcie podanym w systemie
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
